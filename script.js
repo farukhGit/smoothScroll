71ec89880e5363c5b53a7e30c81b367841bb5dd2
farukhGit <furukh4564@gmail.com>
2020-09-23 11:22:22 +0530

    create smooth scroll

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let divBtn = document.querySelectorAll('nav a')
-let currentPosition = window.pageYOffset;
+let currentPosition
 
- let targetPosition = divBtn.forEach(element => {
+let targetPosition = divBtn.forEach(element => {
 
-    element.addEventListener('click', ()=>{
+    element.addEventListener('click', (e)=>{
 
-        return document.querySelector(element.getAttribute('href')).getBoundingClientRect().top
+        e.preventDefault();
+
+        let scrollInterval = setInterval(()=>{
+
+            if(document.querySelector(element.getAttribute('href')).getBoundingClientRect().bottom < window.innerHeight){
+                clearInterval(scrollInterval);
+            }
+            window.scrollBy(0, 5)
+            currentPosition = window.pageYOffset
+        }, 5)        
     })
 })
 
 
 
-// let scrollInterval = setInterval(()=>{
-
-//     if(targetPosition >= currentPosition){
-//         clearInterval(scrollInterval);
-//     }
-//     window.scrollBy(0, 40)
-//     currentPosition += 40
-// }, 20)        
